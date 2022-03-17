2be18509941847fd7ee29220de3c539590c9d98a
Boniface <cbmkboniface@gmail.com>
2022-03-17 11:23:41 +0100

    last updates

--- global_academia.sql
+++ global_academia.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Mar 17, 2022 at 02:20 AM
+-- Generation Time: Mar 17, 2022 at 10:23 AM
 -- Server version: 5.7.34
 -- PHP Version: 7.4.21
 
