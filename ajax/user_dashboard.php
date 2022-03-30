7f47ad4b76bfc1ede674ad3bc3af935d2ab89913
Boniface Mwanza <boniface.mwanza@asml.com>
2022-03-30 02:14:27 +0200

    api fix

--- ajax/user_dashboard.php
+++ ajax/user_dashboard.php
@@ -15,7 +15,7 @@
                 $allProgram->program_id = GetProgramByID($allProgram->program_id);
                 $allProgram->payment_receipt_file = GetMedia( $allProgram->payment_receipt_file);
                 $allProgram->application_status_slug =  __($allProgram->application_status);
-                $addProgram->name = $kd->user->first_name . ' ' . $kd->user->last_name;
+                $allProgram->name = $kd->user->first_name . ' ' . $kd->user->last_name;
             }
             foreach($get_user_notificatins as $nf){
                 if($nf->type == "application_status"){
@@ -801,7 +801,7 @@ if($first  === "delete_agent_application"){
                     $allProgram->university_id = GetuniversityByID($allProgram->university_id);
                     $allProgram->program_id = GetProgramByID($allProgram->program_id);
                     $allProgram->payment_receipt_file = GetMedia( $allProgram->payment_receipt_file);
-                    $addProgram->name = $kd->user->first_name . ' ' . $kd->user->last_name;
+                    $allProgram->name = $kd->user->first_name . ' ' . $kd->user->last_name;
 
                     $allProgram->application_status_slug =  __($allProgram->application_status);
                 }
